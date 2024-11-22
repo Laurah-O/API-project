81e5d171418ad5dd1d35e993a37b1afe54d096a0
Laurah-O <okodelaura720@gmail.com>
2024-11-22 09:46:09 +0300

    Initial commit

---
+++ apiapp/views.py
@@ -0,0 +1,10 @@
+# from django.shortcuts import render
+from .models import Book
+from .serializer import BookSerializer
+from rest_framework import viewsets
+
+# Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset=Book.objects.all()
+    serializer_class=BookSerializer
